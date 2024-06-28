8c2f423bfbe16fb21b84ec8601e3779301baeba4
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-06-28 07:33:06 +0200

    Arrow function

---
+++ JavaScript/ES2015/NodeJs/2-main.js
@@ -0,0 +1,5 @@
+import getNeihborhoodsList from "./2-arrow.js";
+
+const neighborhoodsList = new getNeihborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);

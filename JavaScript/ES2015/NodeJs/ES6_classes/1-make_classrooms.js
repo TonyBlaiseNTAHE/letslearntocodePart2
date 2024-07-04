5d06fa2611798e8b1e1e5dfc41cbb8dd6d3b64b3
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-07-04 09:22:28 +0200

    creating a function named initializeRooms

---
+++ JavaScript/ES2015/NodeJs/ES6_classes/1-make_classrooms.js
@@ -0,0 +1,15 @@
+import {ClassRoom} from './0-classroom.js';
+
+
+export default function initializeRooms(){
+    // creating a array to hold the objects
+    // make a loop that will insert a new object created.
+    const rooms = [];
+    const room_size = [19,20,34];
+
+    for (const i in room_size){
+        const obj = new ClassRoom(room_size[i]);
+        rooms.push(obj);
+    }
+    return rooms;
+}
---
+++ JavaScript/ES2015/NodeJs/ES6_classes/sample.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const a = [1,2,3,4];
+
+console.log(a.length);
